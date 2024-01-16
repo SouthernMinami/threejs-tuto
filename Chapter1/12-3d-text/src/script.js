496ddd391771ba9df167f0940a81654def4eda61
SouthernMinami <kanou373@gmail.com>
2024-01-16 20:25:30 +0900

    1-12: 3d-text

--- Chapter1/12-3d-text/src/script.js
+++ Chapter1/12-3d-text/src/script.js
@@ -1,7 +1,8 @@
+import GUI from 'lil-gui'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import GUI from 'lil-gui'
-
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
 /**
  * Base
  */
@@ -15,19 +16,99 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
+ * Axes Helper
+ * 
+ */
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
+/**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const matcapTexture = textureLoader.load('/textures/matcaps/5.png')
+matcapTexture.colorSpace = THREE.SRGBColorSpace
+console.log(matcapTexture)
+/**
+ * Fonts
+ 
+ */
+const fontLoader = new FontLoader()
+fontLoader.load(
+    '/fonts/helvetiker_regular.typeface.json',
+    (font) => {
+        const textGeometry = new TextGeometry(
+            'Hello Three.js!',
+            {
+                font: font,
+                size: 0.5,
+                height: 0.2,
+                curveSegments: 6,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                bevelSize: 0.02,
+                bevelOffset: 0,
+                bevelSegments: 4
+            }
+        )
+        // use bounding for centering the geometry
+        textGeometry.computeBoundingBox()
+        // Box3 is a class that contains a min and max vector
+        // min property isnt at 0 because of the bevelThickness and bevelSize
+        console.log(textGeometry.boundingBox)
+
+        // textGeometry.translate(
+        //     -(textGeometry.boundingBox.max.x -0.02) * 0.5,
+        //     -(textGeometry.boundingBox.max.y -0.02) * 0.5,
+        //     -(textGeometry.boundingBox.max.z - 0.03) * 0.5  
+        // )
+        // textGeometry.computeBoundingBox()
+        // console.log(textGeometry.boundingBox)
+
+        // more efficient way to center the geometry   
+        textGeometry.center()
+
+        const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture})
+        const text = new THREE.Mesh(textGeometry, material)
+        scene.add(text)
+
+        console.time('donuts')
+        // rendering 100 donuts needs optimization
+        // by move the geometry and material outside the loop
+        // and use the same geometry and material for all donuts        
+        const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+
+        for(let i = 0; i < 1000; i++){
+     
+            const donut = new THREE.Mesh(donutGeometry, material)
+            
+            donut.position.x = (Math.random() - 0.5) * 10
+            donut.position.y = (Math.random() - 0.5) * 10
+            donut.position.z = (Math.random() - 0.5) * 10
+
+            donut.rotation.x = Math.random() * Math.PI
+            donut.rotation.y = Math.random() * Math.PI
+
+            // the random scale needs to be defined in advance
+            const scale = Math.random()
+            donut.scale.set(scale, scale, scale)
+
+            scene.add(donut)
+        }
+
+        console.timeEnd('donuts') // donuts: 25.89111328125 ms
+        // donuts: 2.48681640625 ms with optimization
+    }
+)
 
 /**
  * Object
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial()
+// )
 
-scene.add(cube)
+// scene.add(cube)
 
 /**
  * Sizes
